a67f8691a2bb7ca52e099a766a7f53b38da65a10
BhupendraMessi <076bei011.bhupendra@pcampus.edu.np>
2025-07-18 13:48:50 +0545

    Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
 from .models import Account
+from django.contrib import messages, auth
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -15,7 +18,8 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-
+            messages.success(request, 'Registration successful')
+            return redirect('register')
     else:
         
         form = RegistrationForm()
@@ -28,8 +32,23 @@ def register(request):
 
 
 def login(request):
+    if request.method =="POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            #messages.success(request, "You are logged in")
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'accounts/login.html')
 
-
+@login_required(login_url = 'login')
 def logout(request):
-    return render(request, 'accounts/logout.html')
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
+    
