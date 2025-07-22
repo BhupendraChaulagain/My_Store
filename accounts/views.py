b9f4043d18cbaa3028027d050fab937f2287d283
BhupendraMessi <076bei011.bhupendra@pcampus.edu.np>
2025-07-23 03:38:09 +0545

    Variation Grouping

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,8 @@ from django.contrib import messages, auth
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 
 
 #verification email
@@ -62,6 +64,44 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variation by cart id
+                    product_variation=[]
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #Get the cart items from user to access product variation
+                    cart_item =  CartItem.objects.filter( user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index  = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity +=1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user=user
+                                item.save()
+                        
+                   
+            except:
+                pass
             auth.login(request, user)
             #messages.success(request, "You are logged in")
             return redirect('dashboard')
