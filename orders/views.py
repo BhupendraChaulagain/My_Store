a5a4f018e4f7343a80e31998e8879e9fc765ca82
BhupendraMessi <076bei011.bhupendra@pcampus.edu.np>
2025-08-06 16:16:56 +0545

    Order Completition

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from carts.models import CartItem
 from .forms import OrderForm
 from .models import Order
 import datetime
 import json
 from .models import Payment, OrderProduct
+from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
 
 def payments(request):
     body = json.loads(request.body)
@@ -40,15 +44,34 @@ def payments(request):
 
         cart_item = CartItem.objects.get(id=item.id)
         product_variation = cart_item.variations.all()
-        orderproduct = OrderProduct.objects.get(id = OrderProduct.id)
+        orderproduct = OrderProduct.objects.get(id = orderproduct.id)
         orderproduct.variations.set(product_variation)
         orderproduct.save() 
 
     # Reduce the quantity of the sold products
-    #clear cart
-    # Send order received email to customer
-    #send order number and transaction id back to sendData method via JsonResponse
+        product = Product.objects.get(id = item.product.id)
+        product.stock -= item.quantity
+        product.save()
 
+    #clear cart
+    CartItem.objects.filter(user=request.user).delete()
+
+    # Send order received email to customer
+    mail_subject = 'Thank you for your order!'
+    message = render_to_string('orders/order_received_email.html', {
+        'user': request.user,
+        'order': order,
+    })
+    to_email = request.user.email
+    send_email = EmailMessage(mail_subject, message, to =[to_email])
+    send_email.send()
+
+    #send order number and transaction id back to sendData method via JsonResponse
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id
+    }
+    return JsonResponse(data)
     return render(request, 'orders/payments.html')
 
 
@@ -113,3 +136,28 @@ def place_order(request, total=0, quantity=0):
             return render(request, 'orders/payments.html', context=context)
     else:
         return redirect('store')   
+    
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id = order.id)
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+        payment = Payment.objects.get(payment_id= transID)
+        context = {
+            'order' : order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment.status,
+            'subtotal': subtotal
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except(Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
+
+    
